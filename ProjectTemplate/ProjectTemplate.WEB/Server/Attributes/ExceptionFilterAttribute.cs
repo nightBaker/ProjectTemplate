b5e5b530820b83eda89e492f13a83701a144e089
Yerassyl <e.shalabaiev@temabit.com>
2022-04-10 15:02:36 +0600

    exception filter

--- ProjectTemplate/ProjectTemplate.WEB/Server/Attributes/ExceptionFilterAttribute.cs
+++ ProjectTemplate/ProjectTemplate.WEB/Server/Attributes/ExceptionFilterAttribute.cs
@@ -18,6 +18,7 @@ namespace ProjectTemplate.WEB.Server.Attributes
         private readonly ILogger _logger;
         private JsonSerializerSettings _settings = new JsonSerializerSettings
         {
+            // if we want to deserialize on blazor wasm
             TypeNameHandling = TypeNameHandling.All,
             TypeNameAssemblyFormatHandling = TypeNameAssemblyFormatHandling.Full
         };
@@ -28,21 +29,27 @@ namespace ProjectTemplate.WEB.Server.Attributes
         public override Task OnExceptionAsync(ExceptionContext context)
         {
 
+            context.HttpContext.Response.ContentType = "application/json";
+
             switch (context.Exception)
             {
-                case SomeException someException:                   
-                    context.HttpContext.Response.StatusCode = (int)HttpStatusCode.Conflict; //just as example
+                case SomeException someException:
                     _logger.LogError(context.Exception, nameof(SomeException));
-                    break;
-               
+                    context.HttpContext.Response.StatusCode = (int)HttpStatusCode.Conflict; //just as example                    
+                    return context.HttpContext.Response.WriteAsync(JsonConvert.SerializeObject(context.Exception, _settings));
+                                   
                 default:
                     context.HttpContext.Response.StatusCode = (int)HttpStatusCode.InternalServerError;
                     _logger.LogError(context.Exception, "unhandled exception");
                     break;
             }
 
-            context.HttpContext.Response.ContentType = "application/json";
+#if DEBUG
             return context.HttpContext.Response.WriteAsync(JsonConvert.SerializeObject(context.Exception, _settings));
+#endif
+
+            return context.HttpContext.Response.CompleteAsync();
+
         }
     }
 }
